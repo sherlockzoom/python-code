8096909b0ec41a47933c2c2df96709958280488a
sherlockzoom <seezhouyunli@gmail.com>
2015-10-22 11:16:13 +0800

    add ex31-35

---
+++ learnPythonHardWay/ex34.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding=utf-8
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+print "The animal at 1 is the 2nd animal and is a ",animals[1]
+print "The animal at 2 is the 3rd animal and is a ",animals[2]
+print "The animal at 3 is the 4th animal and is a ",animals[3]
+print "The animal at 4 is the 5th animal and is a ",animals[4]
+print "The animal at 5 is the 6th animal and is a ",animals[5]
+
+print "The 1st animal is at 0 and is a", animals[0]
+print "The 2nd animal is at 1 and is a", animals[1]
+print "The 3rd animal is at 2 and is a", animals[2]
+print "The 4th animal is at 3 and is a", animals[3]
+print "The 4th animal is at 4 and is a", animals[4]
+print "The 5th animal is at 5 and is a", animals[5]
