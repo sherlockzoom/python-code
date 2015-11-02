463f9b040250b9b63a945e8f8a646e678c4dac71
sherlockzoom <seezhouyunli@gmail.com>
2015-11-02 23:09:43 +0800

    add babaynames ex

--- google-python-exercises/babynames/babynames.py
+++ google-python-exercises/babynames/babynames.py
@@ -35,30 +35,74 @@ Suggested milestones for incremental development:
 """
 
 def extract_names(filename):
-  """
-  Given a file name for baby.html, returns a list starting with the year string
-  followed by the name-rank strings in alphabetical order.
-  ['2006', 'Aaliyah 91', Aaron 57', 'Abagail 895', ' ...]
-  """
-  # +++your code here+++
-  return
+    names = []
+    f = open(filename, 'r')
+    text = f.read()
+    year_match = re.search(r'Popularity\sin\s(\d\d\d\d)', text)
+    if not year_match:
+        sys.stderr.write("cannot find year")
+        sys.exit(1)
+    year = year_match.group(1)
+    names.append(year)
+    
+    tuples = re.findall(r'<td>(\d+)</td><td>(\w+)</td><td>(\w+)</td>', text)
+    names_to_rank = {}
+    for rank_tuples in tuples:
+        (rank,boyname,girlname)=rank_tuples
+        if boyname not in names_to_rank:
+          names_to_rank[boyname] = rank
+        if girlname not in names_to_rank:
+          names_to_rank[girlname] = rank
+    sorted_names = sorted(names_to_rank.keys())
+    for name in sorted_names:
+      names.append(name + ' ' + names_to_rank[name])
+    return names
+"""
+Given a file name for baby.html, returns a list starting with the year string
+followed by the name-rank strings in alphabetical order.
+['2006', 'Aaliyah 91', Aaron 57', 'Abagail 895', ' ...]
+"""
+
+      
 
 
 def main():
   # This command-line parsing code is provided.
   # Make a list of command line arguments, omitting the [0] element
   # which is the script itself.
-  args = sys.argv[1:]
+  # args = sys.argv[1:]
 
-  if not args:
-    print 'usage: [--summaryfile] file [file ...]'
-    sys.exit(1)
+  # if not args:
+  #   print 'usage: [--summaryfile] file [file ...]'
+  #   sys.exit(1)
 
-  # Notice the summary flag and remove it from args if it is present.
-  summary = False
-  if args[0] == '--summaryfile':
-    summary = True
-    del args[0]
+  # # Notice the summary flag and remove it from args if it is present.
+  # summary = False
+  # if args[0] == '--summaryfile':
+  #   summary = True
+  #   del args[0]
+  # for filename in args:
+  #   names = extract_names(filename)
+  #   text = '\n'.join(names)
+  #   if summary:
+  #     outf = open(filename+'.summary', 'w')
+  #     outf.write(text+'\n')
+  #     outf.close()
+  #   else:
+  #     print text
+
+  # let's do more
+  import os
+  dir_now = os.listdir('.')
+  for filename in dir_now:
+    if filename[-5:] == '.html':      
+      names = extract_names(filename)
+      text = '\n'.join(names)
+      outf = open(filename+'.summary', 'w')
+      outf.write(text+'\n')
+      outf.close()
+      print text  
+
 
   # +++your code here+++
   # For each filename, get the names, then either print the text output
